0550eeb3c90c27fad81bc0a04abe7570e1bc6e6b
Timothy Corey <me@timothycorey.com>
2016-11-15 11:21:07 -0500

    Initial Application
    Adds the initial version of the application along with updating
    the README file.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# SQLProcessor
+# SQL Processor #
 An application that takes a SQL statement and creates an insert statement from the data returned.
+
+## Usage ##
+To use this application, simply run the EXE file. Once the application loads, it will request your SQL statement (the query that returns a set of data), the table name of the table to insert into, whether you want to include an identity insert, and finally the file you want to output this script to. Fill all of that in, hit the button and your script will be created.
+
+## Roadmap ##
+The following features/tasks need to be completed on this project:
+ 1. Refactor the code to make it efficient and DRY.
+ 2. Move the code to a class library.
+ 3. Upgrade the GUI to be something more pleasant.
+ 4. Add an intermediate step that takes the downloaded data and determines type and allows the user to select or deselect columns.
+ 5. Add an option to open the folder where the script is located.
---
+++ SQLProcessorUI/ProcessorForm.cs
@@ -0,0 +1,113 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLProcessorUI
+{
+    public partial class ProcessorForm : Form
+    {
+        public ProcessorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void createSQLStatementButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            StringBuilder insertStatement = new StringBuilder();
+            IDictionary<string, object> columns = new Dictionary<string, object>();
+
+            if (turnOnIdentityCheckbox.Checked)
+            {
+                insertStatement.Append($"set identity_insert { tableNameText.Text } on;{ Environment.NewLine }");
+            }
+
+            // Starts off the insert statement
+            insertStatement.Append($"insert into { tableNameText.Text } (");
+
+            using (IDbConnection cnn = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                var rows = cnn.Query<dynamic>(sqlStatementText.Text);
+
+                if (rows.Count() > 0)
+                {
+                    columns = (IDictionary<string,object>)rows.First();
+
+                    int i = 0;
+
+                    foreach (var item in columns)
+                    {
+                        if (i > 0)
+                        {
+                            insertStatement.Append(", ");
+                        }
+
+                        insertStatement.Append(item.Key);
+
+                        i += 1;
+                    }
+
+                    if (i == 0)
+                    {
+                        throw new DataException("No valid columns");
+                    }
+
+                    insertStatement.Append($"){ Environment.NewLine }values ");
+                }
+
+                int rowCount = 0;
+
+                // Inserts the actual data
+                foreach (var row in rows)
+                {
+                    var rowVal = (IDictionary<string, object>)row;
+                    int i = 0;
+
+                    if (rowCount > 0)
+                    {
+                        insertStatement.Append(",");
+                    }
+
+                    foreach (var item in columns)
+                    {
+                        if (i == 0)
+                        {
+                            insertStatement.Append($"{ Environment.NewLine }(");
+                        }
+                        else
+                        {
+                            insertStatement.Append(", ");
+                        }
+
+                        insertStatement.Append($"'{ rowVal[item.Key] }'");
+
+                        i += 1;
+                    }
+
+                    insertStatement.Append($")");
+
+                    rowCount += 1;
+                }
+            }
+
+            insertStatement.Append($";{ Environment.NewLine }");
+
+            if (turnOnIdentityCheckbox.Checked)
+            {
+                insertStatement.Append($"set identity_insert { tableNameText.Text } off;{ Environment.NewLine }");
+            }
+
+            File.WriteAllText(outputPathText.Text, insertStatement.ToString());
+
+            MessageBox.Show("Complete");
+        }
+    }
+}
